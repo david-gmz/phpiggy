fb98a9bd72a8af42caf318a86533897f866daf29
David Gómez <david.gmz.b@gmail.com>
2023-09-16 18:43:04 -0300

    Setting the router

---
+++ README.md
@@ -0,0 +1,4 @@
+# A PHP application for tracking expenses
+## An example of a custom framework
+
+From the course **Complete Modern PHP Developer Course in 2023** by Udemy.
---
+++ src/App/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+include __DIR__ . "/../../vendor/autoload.php";
+
+use Framework\App;
+
+$app = new App();
+
+$app->get('/');
+
+dd($app);
+
+return $app;
