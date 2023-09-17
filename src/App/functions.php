12523780f8cbfc7de85b5a86f2d8a2dad06a02d8
David Gómez <david.gmz.b@gmail.com>
2023-09-17 18:13:50 -0300

    TemplateEngine End of section 11

--- src/App/functions.php
+++ src/App/functions.php
@@ -2,10 +2,16 @@
 
 declare(strict_types=1);
 
+// Sugar functions
 function dd(mixed $value)
 {
     echo "<pre>";
     var_dump($value);
     echo "</pre>";
     die();
+}
+
+function e($value)
+{
+    return htmlspecialchars((string) $value);
 }
